70fff4741d45af00a1b6466ea56e9a9f330035d3
Awais <awaiskunjahi1011@gmail.com>
2026-04-10 18:04:52 +0500

    daily practice

---
+++ arrMethod.js
@@ -0,0 +1,55 @@
+// let arr = [1, 2, 3, 4, 5, 7, 8];
+// let result = arr.map((e) => {
+//   return e * null;
+// });
+
+// console.log(result);
+
+// let result = arr.map((e) => {
+//   return e % 2 === 0;
+// });
+
+// console.log(result);
+
+// arr.forEach((e) => {
+//   console.log( e * 2);
+// });
+
+// let sum = arr.reduce((acc, curr) => {
+//   return acc * curr;
+// });
+
+// console.log(sum);
+
+// let nums = [10, 20, 30, 40];
+
+// let res = nums.find((x) => {
+//   return x;
+// });
+
+// console.log(res);
+
+// let arr = [2, 4, 5, 6, 7, "", 8, 4, 2];
+// let result = arr.splice(1, 4, 89);
+// console.log(arr);
+
+// let arr = [1, 2, 3, 4];
+
+// let res = arr.map(x => {
+//   if (x % 2 === 0) return x * 2;
+// });
+
+// console.log(res);
+
+// let arr = [1, 2, 3];
+// let result = [];
+
+// arr.forEach((e) => {
+//   result.push(e * 2);
+// });
+
+// console.log(result);
+
+
+
+
